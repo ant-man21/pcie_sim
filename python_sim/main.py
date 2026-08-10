016ce468dd8abb0f19fb708609cbad4d2f7ad544
Anthony Sanchez <ant21san@gmail.com>
2026-08-10 14:42:43 -0500

    python sim enumuration

--- python_sim/main.py
+++ python_sim/main.py
@@ -1,51 +1,37 @@
 import config_space as cs
 
-def main():
-	sim = cs.ConfigSpace("topology.yaml")
-	vendor_id = sim.read_config(0, 0, 0, cs.VENDOR_ID, 2) #root port 0
-	print(hex(vendor_id))
-	sim.write_config(0, 0, 0, cs.SECONDARY_BUS, 1, 1)
-	vendor_id = sim.read_config(1, 0, 0, cs.VENDOR_ID, 2) #switch 0
-	print(hex(vendor_id))
 
 
-
-	sim.write_config(1, 0, 0, cs.SECONDARY_BUS, 1, 2)
-
-	next_bus = 3
-	for device in (0, 1, 2):
-		vendor_id = sim.read_config(2, device, 0, cs.VENDOR_ID, 2)
-		if vendor_id == 0xFFFF:
-			print(str(device) + " no device")
+def enumerate_pcie(curr_bus, next_bus, sim):
+	
+	for device in range(32):
+		# print(curr_bus)
+		if curr_bus == 255:
+			return # too many buses. might be a dead check
+		vendor_id = sim.read_config(curr_bus, device, 0, cs.VENDOR_ID, 2)
+		if vendor_id == 0xffff:
 			continue
+		else:
+			#real device
+			header_type = sim.read_config(curr_bus, device, 0, cs.HEADER_TYPE, 2)
+			print(f"bus{curr_bus}:dev{device}: venid: {hex(vendor_id)} header_type: {hex(header_type)}")
+			if header_type == 0x01: #bus
+				assigned_bus = next_bus
+				sim.write_config(curr_bus, device, 0, cs.SECONDARY_BUS, 1, next_bus)
+				next_bus = assigned_bus + 1
+				next_bus = enumerate_pcie(assigned_bus, next_bus, sim)
+			else: #header_type == 0x00 probably?
+				base_class = sim.read_config(curr_bus, device, 0, cs.BASE_CLASS, 2)
+				sub_class = sim.read_config(curr_bus, device, 0, cs.SUBCLASS, 2)
+				print(f"bus{curr_bus}:dev{device}: venid: {hex(vendor_id)} class: {hex(base_class)}:{hex(sub_class)} ")
+				pass
+	return next_bus
 
-		header_type = sim.read_config(2, device, 0, cs.HEADER_TYPE, 1)
-		print(f"dev{device}: vendor={hex(vendor_id)} header_type={hex(header_type)}")
-
-		if header_type != 0x01:
-			continue		  # it's an endpoint -- nothing behind it, nothing to assign
-
-		assigned_bus = next_bus
-		sim.write_config(2, device, 0, cs.SECONDARY_BUS, 1, assigned_bus)
-		next_bus += 1
-		for j in range(0, 32):
-			v = sim.read_config(assigned_bus, j, 0, cs.BASE_CLASS, 2)
-			if v == 0xFFFF:
-				break
-			print(f"bus{assigned_bus}: dev{j}: v={hex(v)}")
-
-	# vendor_id = sim.read_config(0, 1, 0, cs.VENDOR_ID, 2)  # root_port 1
-	# print(hex(vendor_id))
-	# sim.write_config(0, 1, 0, cs.SECONDARY_BUS, 1, 5)
-	# vendor_id = sim.read_config(5, 0, 0, cs.BASE_CLASS, 2)  # root_port 1
-	# print(hex(vendor_id))
-
-	# vendor_id = sim.read_config(0, 2, 0, cs.VENDOR_ID, 2)  # root_port 1
-	# print(hex(vendor_id))
-	# sim.write_config(0, 2, 0, cs.SECONDARY_BUS, 1, 6)
-	# vendor_id = sim.read_config(6, 0, 0, cs.BASE_CLASS, 2)  # root_port 1
-	# print(hex(vendor_id))
-
+def main():
+	curr_bus = 0
+	next_bus = curr_bus + 1
+	sim = cs.ConfigSpace("topology.yaml")
+	enumerate_pcie(curr_bus, next_bus, sim)
 
 if __name__ == '__main__':
 	main()
